cbf68fa164dda563fd958ad93db2a395a1516f48
AnujChhikara <anujchhikara777@gmail.com>
2024-11-09 11:38:18 +0530

    implemented basic dashboard page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,8 @@ import Home from "./Pages/Home";
 import NotFound from "./Pages/NotFound";
 import LoginPage from "./Pages/Login";
 import RegisterPage from "./Pages/Register";
-
+import Dashboard from "./Pages/Dashboard";
+import { Toaster } from "@/components/ui/toaster";
 const AllRoutes = () => {
   return (
     <BrowserRouter>
@@ -13,6 +14,7 @@ const AllRoutes = () => {
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
         <Route path='*' element={<NotFound />} />
+        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
@@ -21,6 +23,7 @@ function App() {
   return (
     <div className='App'>
       <AllRoutes />
+      <Toaster />
     </div>
   );
 }
